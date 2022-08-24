99dc26261626cc9693ee25a3ebb4524319730976
PierDun <kuznetsoffar@ya.ru>
2022-08-24 22:08:17 +0300

    add fetch api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { log } from 'console';
 
 function App() {
+
+interface IProduct {
+  category: string
+  description: string
+  id?: number
+  image: string
+  price: number
+  title: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
+const [data, setData] = useState({
+  category: null,
+  description: null,
+  price: null,
+  loader: true
+})
+
+  let URL: string = 'https://fakestoreapi.com/products/3';
+
+  fetch(URL)
+  .then((res) => res.json())
+  .then(body => 
+    // console.log(body.category)
+    // console.log(body.category);
+    setData({
+      category: body.category,
+      description: body.description,
+      price: body.price,
+      loader: false
+    }
+  ))
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +55,15 @@ function App() {
           Learn React
         </a>
       </header>
+      
+      <h1>qqq</h1>
+
+      {data.loader ? <p>loading</p>: <ul>
+        <li>{data.category}</li>
+        <li>{data.description}</li>
+        <li>{data.price}</li>
+      </ul>}
+      
     </div>
   );
 }
